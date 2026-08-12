f4d56f112807ebc2cf2ae506eb0c7afe500eb680
Jackson <porccode@gmail.com>
2026-08-12 17:06:20 -0300

    Mais desestruturação

--- Revisão JavaScript/objetos.js
+++ Revisão JavaScript/objetos.js
@@ -1,2 +1,9 @@
-const uppterName = (name) => name.toUpperCase();
-console.log(uppterName('Jackson'));
+// const uppterName = (name) => name.toUpperCase();
+// console.log(uppterName('Jackson'));
+
+function handleMouse(event) {
+  const { clientX, clientY } = event;
+  console.log(clientX, clientY);
+}
+
+document.addEventListener('click', handleMouse);
